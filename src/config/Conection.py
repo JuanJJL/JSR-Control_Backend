3b3a065b08fda45a0df3b9ab48a33a82a941f226
LianzJJL <juanjalo111@gmail.com>
2025-11-04 15:17:39 -0500

    Se añadio la conexion a la base de datos

---
+++ src/config/Conection.py
@@ -0,0 +1,16 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from libsql_client import create_client
+
+load_dotenv()
+
+URL_TURSO = os.getenv("TURSO_DATABASE_URL")
+TOKEN_TURSO = os.getenv("TURSO_AUTH_TOKEN")
+
+
+def Conection():
+    client = create_client(url=URL_TURSO, auth_token=TOKEN_TURSO)
+    return client
+
+
