00bf69c287f22dd983bb8bc8cfa564991cf88ead
Superdroid7 <samuel.cardona.z97@gmail.com>
2025-11-24 22:48:07 -0500

    renzo es gay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from src.routes.UserRoutes import router_users
 from src.routes.AuthRoutes import router
+from src.routes.ProductRoutes import router_products
 
 
 
@@ -8,3 +9,4 @@ app = FastAPI()
 
 app.include_router(router_users, prefix="/users", tags=["users"])
 app.include_router(router, prefix="/auth", tags=["auth"])
+app.include_router(router_products, prefix="/products", tags=["products"])
